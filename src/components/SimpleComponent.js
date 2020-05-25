bf7c38d7affe23ab88f0ea36bb54113b20c1830f
Leah Silber <LeahSilber@gmail.com>
2020-05-24 19:54:39 -0700

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
-// Code SimpleComponent Here
+// @ts-check
+import React from "react";
+
+export default class SimpleComponent extends React.Component {
+  state = {
+    mood: "happy",
+  };
+
+  handleClick = (event) => {
+    this.state.mood === "happy"
+      ? this.setState({ mood: "sad" })
+      : this.setState({ mood: "happy" });
+  };
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
